e28f4b1cb042b402304daafd7fd26f2a58ab4ca8
Klüpfel Annika <AnnikaKluepfel@web.de>
2022-05-21 22:54:05 +0200

    use pymongo to add comments to db. Add try/except block for browser driver

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from mongodb import add_entries
 import time
-import json
 
 # define the driver to use and the page to scrape.
 # Selenium supports various browsers/drivers, I'm using the one for Firefox
@@ -10,12 +10,15 @@ driver = webdriver.Firefox(executable_path = path)
 main_url = "https://ec.europa.eu/info/law/better-regulation/have-your-say/initiatives/13375-Extension-of-EU-Digital-COVID-Certificate-Regulation/feedback_de?p_id=27926341"
 
 # run the driver, access the page, and get its HTML
-driver.get(main_url)
-time.sleep(3)
+try:
+    driver.get(main_url)
+    time.sleep(3)
 
-html = driver.page_source
-pageSoup = BeautifulSoup(html, "html.parser")
-driver.close()
+    html = driver.page_source
+    pageSoup = BeautifulSoup(html, "html.parser")
+    driver.close()
+except Exception as e:
+    print(e)
 
 # first, let's find the last comment page from the pagination items
 def getLastPage(soup):
@@ -31,22 +34,27 @@ load_index = 0
 
 # when the comment section was still open, new comments got added to the first page,
 # which made it difficult to retrieve them without accidentally skipping content.
-# Now, the comment section is closed so we can simply start scraping on page one: 
 
+# Meanwhile, the comment section has been closed 
+# so we can simply start scraping on page one and continue up until the last page: 
 while load_index <= last_page_index:
+    page_no = load_index + 1
     page_comments = []
+
     # opening the driver and having it access the page...
     print(f"loading page {load_index + 1}")
     url = f"{main_url}&page={load_index}"
     driver = webdriver.Firefox(executable_path = path)
-    driver.get(url)
-    # loading the comment section is quite slow,
-    # so I make sure to wait long enough before getting the content
-    time.sleep(9) 
-    
-    html = driver.page_source
-    soup = BeautifulSoup(html, features="html.parser")
-    driver.close()
+    try:
+        driver.get(url)
+        # loading comments is slow so I make sure to wait a few seconds before getting the content
+        time.sleep(9) 
+        
+        html = driver.page_source
+        soup = BeautifulSoup(html, features="html.parser")
+        driver.close()
+    except Exception as e:
+        print(e)
 
     # now we get all names, comment texts, and submit dates...
     names = soup.find_all('div', {'class' : 'ecl-u-type-prolonged-m'})
@@ -60,16 +68,19 @@ while load_index <= last_page_index:
         date_and_time = datetime.rsplit(" ")
         comment = {
             "page_index": load_index,
-            "page_no": load_index + 1,
+            "page_no": page_no,
             "comment_no": (load_index * 10) + i + 1,
             "name": names[i].text,
             "date": date_and_time[0],
             "time": date_and_time[1],
             "comment": comment_texts[i].text
         }
-        # add comment to the page comments and general comments lists
+        # add comment to the page_comments list
         page_comments.append(comment)
         i += 1
+    
+    # add page comments to db
+    add_entries(page_comments, page_no)
 
     # okay, that's it for this page. Let's increase our index so we can get the next page's content!
     load_index += 1
---
+++ mongodb.py
@@ -0,0 +1,16 @@
+from pymongo.mongo_client import MongoClient
+
+# connecting to a local mongoDB instance
+my_url = "mongodb://localhost:27017"
+client = MongoClient(my_url, serverSelectionTimeoutMS=5000)
+
+db = client["ec_regulation"]
+col = db["comments"]
+
+# defining db method
+def add_entries(entries, page):
+    try:
+        col.insert_many(entries)
+        print(f"inserted entries of page {page} to db")
+    except Exception as e:
+        print(e.details)
