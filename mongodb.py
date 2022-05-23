6d8e2256037c4f401869159897563e08670bbc2b
Klüpfel Annika <AnnikaKluepfel@web.de>
2022-05-23 18:46:36 +0200

    err log for add_entries, method to retrieve content grouped by page index

--- mongodb.py
+++ mongodb.py
@@ -13,4 +13,23 @@ def add_entries(entries, page):
         col.insert_many(entries)
         print(f"inserted entries of page {page} into db")
     except Exception as e:
-        print(e.details)
+        print(e)
+        with open('error_logs.txt', 'w') as log:
+            log.write(f"an exception occurred on page {page} (index {page - 1})")
+            log.write(str(e))
+            log.close()
+
+def comments_per_page(last_index):
+    pipeline=[{ "$group": {"_id" : "$page_index", "comments": { "$push": "$comment_no" } }}]
+    pages = list(col.aggregate(pipeline))
+    i = 0
+    while i <= last_index:
+        try:
+            index = list(filter(lambda index: index["_id"] == i, pages))
+            if index == []:
+                print(f"no page for index {i}")
+            i+=1
+        except Exception as e:
+            print(f"error loading index {i}")
+            print(e)
+            i+=1
